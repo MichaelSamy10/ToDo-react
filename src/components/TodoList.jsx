9024a31a04bc5e6c3747884dfff778c158d755d3
Michael Samy <michaelsamy196@gmail.com>
2024-07-04 12:51:19 +0300

    add style

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -17,15 +17,20 @@ function TodoList() {
 
   return (
     <div>
-      <div className="m-5">
+      <div className=" m-5">
         <h2>To Do List</h2>
-        <div className=" container border rounded w-50">
+        <div className="bg-secondary-subtle container border border-black rounded w-50">
           <div className="row m-5">
             <div className="input-group">
               <span className="input-group-text">
                 <IoDocumentTextOutline />
               </span>
-              <input type="text" className="form-control" ref={inputRef} />
+              <input
+                type="text"
+                placeholder="Enter item..."
+                className="form-control"
+                ref={inputRef}
+              />
               <span className="btn btn-primary" onClick={addTodoHandler}>
                 Add
               </span>
